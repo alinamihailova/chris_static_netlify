8a5cb21ad19e32e7d95fdab63df9958019fd7523
Alina Mihailova <fhs47428@fh-salzburg.ac.at>
2025-10-15 15:47:08 +0200

    Initial commit

---
+++ clean-build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -rf public/ && mkdir public
+./build.sh
+
---
+++ src/js/mobile-menu.js
@@ -0,0 +1,21 @@
+const mobileMenu = () => {
+    const nav = document.querySelector('#mainnav')
+    const list = nav.querySelector('ul');
+    const button = document.querySelector('#hamburger');
+
+    button.addEventListener('click', e => {
+        if (button.classList.contains('mobile-menu-open')) {
+            button.classList.remove('mobile-menu-open');
+        } else {
+            button.classList.add('mobile-menu-open');
+        }
+    });
+
+    nav.addEventListener('keyup', e => {
+        if (e.code === 'Escape') {
+            button.classList.remove('mobile-menu-open');
+        }
+    });
+}
+
+export default mobileMenu;
