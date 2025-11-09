422b737372319c68636bc76495b9b489e5a87600
Alina Mihailova <alina2233223@gmail.com>
2025-11-09 22:49:57 +0100

    adding favicon and ogimage

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cp ./src/index.html ./public/
 npx sass ./src/sass/style.scss ./public/style.css
 npx esbuild ./src/js/main.js --bundle --outfile=./public/main.js --minify
-cp ./src/favicons/*.* ./public/
+cp ./src/favicon/*.* ./public/
 cp -R ./src/images ./public/
 cp -R ./src/fonts ./public/
 
